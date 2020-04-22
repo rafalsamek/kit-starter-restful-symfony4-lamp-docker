29168807f91a7a07eca1529dae95c50b11e59eb8
Rafal Samek <rafal.samek@gmail.com>
2020-04-23 00:47:17 +0200

    - App\Controller\Rest\ArticleController - created - App\Tests\Controller\Rest\ArticleControllerTest - created - TODO list for TDD, starting point

---
+++ app/src/Controller/Rest/ArticleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Rest;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+
+class ArticleController extends AbstractFOSRestController
+{
+
+}
---
+++ app/tests/Controller/Rest/ArticleControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller\Rest;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * TODO:
+ *  App\Controller\Rest\ArticleController - Article RESTful API in TDD
+ *  (App\Tests\Controller\Rest\ArticleControllerTest corresponding unit tests):
+ *      1.0:
+ *          POST    /api/articles               - create article, no error handling
+ *          GET     /api/articles/{articleId}   - read one article by articleId, no error handling
+ *          GET     /api/articles               - read list of articles, no filters,
+ *                                                no sort by, no pagination, no error handling
+*           PUT     /api/articles/{articleId}   - update article by articleId, no error handling
+ *          DELETE  /api/articles/{articleId}   - DELETE one by articleId, no error handling
+ *      1.1:
+ *          add error handling to all RESTful verbs - above actions
+ *      2.0:
+ *          GET     /api/articles/              - read list of articles with filters, sort by,
+ *                                                pagination and of course (as of 1.1) error handling
+*/
+class ArticleControllerTest extends TestCase
+{
+
+}
